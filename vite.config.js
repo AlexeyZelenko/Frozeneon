6961a01ce7daa3f157703d745790cab399c54285
Alexey Zelenko <zelenlooleksii.75@gmail.com>
2025-02-22 15:29:13 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
@@ -9,4 +10,5 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    base: '/Frozeneon/'
 });
